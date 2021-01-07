16e1a95dc7780de537a23a08382a4b14614b4498
Shogo <vanitas710@yahoo.co.jp>
2021-01-08 04:50:23 +0900

    1st commit

--- UITableView/ViewController.swift
+++ UITableView/ViewController.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let sectionTitle = ["チョウ目","バッタ目","コウチュウ目"]
+let section0 = [("キタテハ","タテハチョウ科"),("クロアゲハ","アゲハチョウ科")]
+let section1 = [("キリギリス","キリギリス科"),("ヒナバッタ","バッタ科"),("マツムシ","ナツムシ科")]
+let section2 = [("ハンミョウ","ハンミョウ科"),("アオオサムシ","オサムシ科"),("チビクワガタ","クワガタムシ科")]
+let tableData = [section0,section1,section2]
 
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionData = tableData[section]
+        return sectionData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let sectionData = tableData[(indexPath as NSIndexPath).section]
+        let cellData = sectionData[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = cellData.0
+        cell.detailTextLabel?.text = cellData.1
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitle[section]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+        
+        let myTableView = UITableView(frame: view.frame,style: .grouped)
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        view.addSubview(myTableView)
+        
     }
 
 
